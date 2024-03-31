afec2d18b3fe214bea8d338de0f8695c5f777a97
Naman <njudaipur04@gmail.com>
2024-03-31 14:14:27 +0530

    Submit Message

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let submit = document.querySelector('#contact form');
+
+function formSubmit(){
+
+    alert("Thank You for your response. We will get back to you soon!")
+}
+
+submit.addEventListener("submit" , formSubmit());
