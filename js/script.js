93e1544f851dcda5392c857ccf153f701b769e36
Naman <njudaipur04@gmail.com>
2024-03-31 18:43:07 +0530

    JS added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
-let submit = document.querySelector('#contact form');
+const submit = document.querySelector('#contact form');
+const about = document.querySelector('#readMore');
+const aboutPara = document.querySelector('#aboutPara a');
 
 function formSubmit(){
-
-    alert("Thank You for your response. We will get back to you soon!")
+    alert("Thank You for your response. We will get back to you soon!");
 }
 
-submit.addEventListener("submit" , formSubmit());
+
+function readMore(){
+    console.log("readMore function called");
+    if(about.style.display === "none"){
+        console.log("about is hidden, so showing it");
+        about.style.display = "block";
+        aboutPara.innerHTML = "Read Less...";
+    }
+    else{
+        console.log("about is visible, so hiding it");
+        about.style.display = "none";
+        aboutPara.innerHTML = "Read More...";
+    }
+    console.log("readMore function ended");
+}
+
+submit.addEventListener("submit" , formSubmit);
+aboutPara.addEventListener("click" , readMore);
